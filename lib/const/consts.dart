a6b03af355f397fb1a98c3c5f5ba30672172b229
RJ2607 <rishijain2607@gmail.com>
2023-05-23 23:29:30 +0530

    UI

---
+++ lib/const/consts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const bgColor = Color(0xff2C3333);
+  static const textColor = Color(0xffE7F6F2);
+}
+
+class GameUI {
+  static const a0 = "assets/0.png";
+  static const a1 = "assets/1.png";
+  static const a2 = "assets/2.png";
+  static const a3 = "assets/3.png";
+  static const a4 = "assets/4.png";
+  static const a5 = "assets/5.png";
+  static const a6 = "assets/6.png";
+}
--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -10,11 +10,48 @@ class Gamescreens extends StatefulWidget {
 }
 
 class _GamescreensState extends State<Gamescreens> {
+  var characters = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
-    );
+        appBar: AppBar(
+          elevation: 0.0,
+          title: Text("HANGMAN: THE WORD GAME"),
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+        ),
+        body: Column(
+          children: [
+            Expanded(
+                child: Column(
+              children: [
+                Expanded(flex: 4, child: Container(color: Colors.amber)),
+                Expanded(child: Container(color: Colors.green)),
+              ],
+            )),
+            Expanded(
+                child: Container(
+              padding: const EdgeInsets.all(8),
+              child: GridView.count(
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 6,
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 4,
+                children: characters.split("").map((e) {
+                  return ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      e,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                  );
+                }).toList(),
+              ),
+            ))
+          ],
+        ));
   }
 }
