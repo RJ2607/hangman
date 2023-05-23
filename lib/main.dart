3257d37829f02c5a13be5981014b9c5f8eeb451e
RJ2607 <rishijain2607@gmail.com>
2023-05-23 22:15:55 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
-o
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
