84451b11ea2b6ab11dc2d64f577d6cc2da911c90
RJ2607 <rishijain2607@gmail.com>
2023-05-23 23:58:05 +0530

    game UI

--- lib/game_screen.dart
+++ lib/game_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:hangman/game/hidden_letters.dart';
 
 class Gamescreens extends StatefulWidget {
   const Gamescreens({super.key});
@@ -11,6 +12,9 @@ class Gamescreens extends StatefulWidget {
 
 class _GamescreensState extends State<Gamescreens> {
   var characters = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+  var word = "baaba".toUpperCase();
+  List<String> selectedchar = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,34 +27,47 @@ class _GamescreensState extends State<Gamescreens> {
         body: Column(
           children: [
             Expanded(
+                flex: 3,
                 child: Column(
-              children: [
-                Expanded(flex: 4, child: Container(color: Colors.amber)),
-                Expanded(child: Container(color: Colors.green)),
-              ],
-            )),
+                  children: [
+                    Expanded(flex: 4, child: Container(color: Colors.amber)),
+                    Expanded(
+                        child: Container(
+                      padding: const EdgeInsets.all(8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: word
+                            .split("")
+                            .map((e) => hiddenLetter(
+                                e, !selectedchar.contains(e.toUpperCase())))
+                            .toList(),
+                      ),
+                    )),
+                  ],
+                )),
             Expanded(
+                flex: 2,
                 child: Container(
-              padding: const EdgeInsets.all(8),
-              child: GridView.count(
-                physics: NeverScrollableScrollPhysics(),
-                crossAxisCount: 6,
-                crossAxisSpacing: 4,
-                mainAxisSpacing: 4,
-                children: characters.split("").map((e) {
-                  return ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      e,
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    style:
-                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                  );
-                }).toList(),
-              ),
-            ))
+                  padding: const EdgeInsets.all(8),
+                  child: GridView.count(
+                    physics: NeverScrollableScrollPhysics(),
+                    crossAxisCount: 7,
+                    crossAxisSpacing: 4,
+                    mainAxisSpacing: 4,
+                    children: characters.split("").map((e) {
+                      return ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          e,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.black),
+                      );
+                    }).toList(),
+                  ),
+                ))
           ],
         ));
   }
