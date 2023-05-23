6d009ec3fc4141478411a1716fa6b774c9035f6a
RJ2607 <rishijain2607@gmail.com>
2023-05-23 22:32:39 +0530

    game screen

---
+++ lib/game_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Gamescreens extends StatefulWidget {
+  const Gamescreens({super.key});
+
+  @override
+  State<Gamescreens> createState() => _GamescreensState();
+}
+
+class _GamescreensState extends State<Gamescreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:hangman/game_screen.dart";
 
 void main() {
   runApp(MyApp());
@@ -9,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Gamescreens(),
+    );
   }
 }
